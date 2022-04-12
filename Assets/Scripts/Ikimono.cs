e69a3e2ad39575fe33d5dcfa4341ee31fa03470a
LiterallyFabian <lindgrenfabian@gmail.com>
2022-04-12 17:19:14 +0200

    Added hello

--- Assets/Scripts/Ikimono.cs
+++ Assets/Scripts/Ikimono.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hi");
     }
 
     // Update is called once per frame
