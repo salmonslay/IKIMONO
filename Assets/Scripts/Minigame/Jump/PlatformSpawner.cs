2a57beb3e2d647c17bceab1efccb30e5eb804e4f
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-04-24 10:20:52 +0200

    Add platform prefab

--- Assets/Scripts/Minigame/Jump/PlatformSpawner.cs
+++ Assets/Scripts/Minigame/Jump/PlatformSpawner.cs
@@ -10,15 +10,21 @@ namespace IKIMONO.Minigame.Jump
     {
         [SerializeField] private GameObject _platformPrefab;
         
-        [Header("Platform Spawning")]
+        [Header("Platforms")]
         
         [Tooltip("The odds of a platform spawning per meter")]
-        [SerializeField] private float _initialSpawnRate = 70f;
+        [SerializeField] private float _initialSpawnRate = 0.7f;
         
         [Tooltip("The minimum odds at which a platform will spawn")]
-        [SerializeField] private float _minSpawnRate = 5f;
+        [SerializeField] private float _minSpawnRate = 0.05f;
+        
+        [Tooltip("The initial width of the platform")]
+        [SerializeField] private float _initialWidth = 1.5f;
+        
+        [Tooltip("The minimum width at which a platform will spawn")]
+        [SerializeField] private float _minWidth = 0.5f;
 
-        [Tooltip("The Y point where the lowest odds will be reached")] 
+        [Tooltip("The Y point where the lowest spawning odds and platform width will be reached")] 
         [SerializeField] private float _finalPoint = 500f;
         
         /// <summary>
@@ -28,11 +34,22 @@ namespace IKIMONO.Minigame.Jump
 
         private void Update()
         {
-            // TODO: Run the odds for spawning a platform if the player is close enough away to the last one spawned
+            // TODO Tova:
+            // - If the player is close enough to the last spawned meter, run SpawnPlatform(_lastSpawnedMeter)
         }
 
-        private void SpawnPlatform()
+        private void SpawnPlatform(int y)
         {
+            // TODO Tova:
+            // - Check if this meter has already been spawned (if so, don't spawn)
+            // - Run the odds for spawning a platform from GetCurrentSpawnRate()
+            // - If true, spawn a platform, else return
+            
+            // - Instantiate the platform (_platformPrefab)
+            // - Set the width of the platform from GetCurrentWidth()
+            // - Set the X position of the platform to a random value within the border
+            // - Set the Y position of the platform to the Y value passed in 
+            
             throw new NotImplementedException("SpawnPlatform not implemented");
         }
 
@@ -42,6 +59,13 @@ namespace IKIMONO.Minigame.Jump
             // The spawn rate will go from _initialSpawnRate to _minSpawnRate as the player progresses towards the _finalPoint
             float percent = Math.Min(JumpManager.Instance.HighestJump / _finalPoint, 1);
             return Mathf.Clamp(_initialSpawnRate - (_initialSpawnRate - _minSpawnRate) * percent, _minSpawnRate, _initialSpawnRate);
+        }
+        
+        private float GetCurrentWidth()
+        {
+            // The math is the same as above, but the width will go from _initialWidth to _minWidth
+            float percent = Math.Min(JumpManager.Instance.HighestJump / _finalPoint, 1);
+            return Mathf.Clamp(_initialWidth - (_initialWidth - _minWidth) * percent, _minWidth, _initialWidth);
         }
         
     }
