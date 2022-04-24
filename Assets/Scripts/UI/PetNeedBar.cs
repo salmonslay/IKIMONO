01f791daaa7da69db981b09ff4215bd7861d87fc
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-04-24 17:39:56 +0200

    Implement proof of concept main screen

---
+++ Assets/Scripts/UI/PetNeedBar.cs
@@ -0,0 +1,29 @@
+using IKIMONO.Pet;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IKIMONO.UI
+{
+    public class PetNeedBar : MonoBehaviour
+    {
+        [SerializeField] private Image _fillImage;
+        private PetNeed _petNeed;
+
+        private void Awake()
+        {
+            PetNeed.ValueUpdated += UpdateValue;
+        }
+
+        public void SetNeed(PetNeed need)
+        {
+            _petNeed = need;
+        }
+
+        private void UpdateValue()
+        {
+            if (_petNeed == null) return;
+            
+            _fillImage.fillAmount = _petNeed.Percentage;
+        }
+    }
+}
