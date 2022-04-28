d1ff1f43891fc82efbdbe56b7f5a9c72362eb695
Elland12 <103199203+Elland12@users.noreply.github.com>
2022-04-28 11:37:25 +0200

    klicktest test

--- Assets/Klicker.cs
+++ Assets/Klicker.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Klicker : MonoBehaviour
 {
+    // För att kalla in ljud skriv : FindObjectOfType<AudioManager>().playSound("LjudetsKey", "LjudetsNamn");
+    // För att randomizea en ljudlista skriv : FindObjectOfType<AudioManager>().randomizeSound("LjudetsKey");
+
     // Start is called before the first frame update
     void Start()
     {
-        FindObjectOfType<AudioManager>().playSound("Music", "ett");
-        FindObjectOfType<AudioManager>().playSound("DayAmb", "ett");
+        FindObjectOfType<AudioManager>().playSound("Music", "Two");
+        FindObjectOfType<AudioManager>().playSound("DayAmb", "One");
     }
 
     // Update is called once per frame
@@ -18,6 +21,7 @@ public class Klicker : MonoBehaviour
         {
            FindObjectOfType<AudioManager>().randomizeSound("Happy");
            FindObjectOfType<AudioManager>().randomizeSound("Eating");
+           FindObjectOfType<AudioManager>().playSound("Button", "One"); 
             
             Debug.Log("Spacetestknappfungerar");
         }
