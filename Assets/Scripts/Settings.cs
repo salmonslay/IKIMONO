74ae9938dbc70c212c19ec0bdaa7bdd07c2de3a1
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-04-29 22:56:08 +0200

    Make settings opt-in to json

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,8 +1,7 @@
 using Newtonsoft.Json;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[JsonObject(MemberSerialization.OptIn)]
 public class Settings
 {
     [JsonProperty("musicVolume")] private float _musicVolume = 1;
@@ -11,19 +10,12 @@ public class Settings
     public float MusicVolume
     {
         get => _musicVolume;
-        set
-        {
-            _musicVolume = Mathf.Clamp(value, 0.0001f, 1);
-        }
+        set => _musicVolume = Mathf.Clamp(value, 0.0001f, 1);
     }
 
     public float EffectsVolume
     {
         get => _effectsVolume;
-        set
-        {
-            _effectsVolume = Mathf.Clamp(value, 0.0001f, 1);
-        }
+        set => _effectsVolume = Mathf.Clamp(value, 0.0001f, 1);
     }
-
 }
