b6b035ed4996c256130d4cf88c4d0cd10296b366
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-04-30 18:19:30 +0200

    Show coins in main

---
+++ Assets/Scripts/UI/CoinCounter.cs
@@ -0,0 +1,22 @@
+﻿using IKIMONO.Pet;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IKIMONO.UI
+{
+    // TODO: Replace with event system
+    public class CoinCounter : MonoBehaviour
+    {
+        private Text _text;
+        
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+        }
+        
+        private void Update()
+        {
+            _text.text = Player.Instance.Coins.ToString();
+        }
+    }
+}
