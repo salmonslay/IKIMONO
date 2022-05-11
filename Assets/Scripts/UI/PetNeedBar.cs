c0d6350f67f4612f96d2ab97e28255b1049d429d
TriKnot <90341216+TriKnot@users.noreply.github.com>
2022-05-11 22:26:44 +0200

    Added functionality to show arrows on all need buttons
    Also reworked how sleeping arrow is handled.

--- Assets/Scripts/UI/PetNeedBar.cs
+++ Assets/Scripts/UI/PetNeedBar.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using IKIMONO.Pet;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,16 +10,17 @@ namespace IKIMONO.UI
     {
         [Tooltip("The image to fill with the pet's need.")]
         [SerializeField] private Image _fillImage;
-        
+
         [Tooltip("The gradient to use for the fill image.")]
         [SerializeField] private Gradient _gradient;
-        
+
         [Tooltip("Whether or not the pet's name should be displayed on the bar.")]
         [SerializeField] private bool _showName;
 
         [SerializeField] private Image _arrow;
-        
+
         private PetNeed _petNeed;
+        private PetNeedEnergy _petNeedEnergy;
         private Button _button;
 
         private void Awake()
@@ -28,22 +30,70 @@ namespace IKIMONO.UI
 
             if (_showName)
                 GetComponentInChildren<Text>().text = Player.Instance.Pet.Name;
-            
+
             _button = GetComponent<Button>();
+
+            _petNeedEnergy = Player.Instance.Pet.Energy;
+        }
+
+        private void Start()
+        {
+            if (typeof(PetNeedEnergy) == _petNeed.GetType())
+            {
+                SetArrowState(_petNeedEnergy.IsSleeping);
+            }
         }
 
         private void Update()
         {
-            // If the pet is sleeping, show the arrow.
-            if (typeof(PetNeedEnergy) == _petNeed.GetType())
+            _button.interactable = _petNeed.UsageCondition;
+        }
+
+        /// <summary>
+        /// Turns on or off arrow based on passed parameter; 
+        /// </summary>
+        /// <param name="isTurnedOn"></param>
+        public void SetArrowState(bool isTurnedOn)
+        {
+            _arrow.enabled = isTurnedOn;
+        }
+
+        private Animator _animator;
+        private bool _canPlayAgain = true;
+        [SerializeField] private Color _upArrowColor = new Color(179, 255, 165);
+        [SerializeField] private Color _downArrowColor = new Color(179, 255, 165);
+        /// <summary>
+        /// Shows positive/negative arrow over button once based on passed bool parameter.
+        /// </summary>
+        /// <param name="isPositive"></param>
+        public void ShowArrow(bool isPositive)
+        {
+            _animator = _arrow.gameObject.GetComponent<Animator>();
+
+            if (_canPlayAgain)
             {
-                _arrow.enabled = Player.Instance.Pet.Energy.IsSleeping;
+                if (isPositive)
+                {
+                    _arrow.gameObject.transform.localScale = new Vector3(1, 1, 1);
+                    _arrow.color = _upArrowColor;
+                }
+                else
+                {
+                    _arrow.gameObject.transform.localScale = new Vector3(1, -1, 1);
+                    _arrow.color = _downArrowColor;
+                }
+                StartCoroutine(ShowArrowEnumerator());
             }
-            
-            else
-            {
-                _button.interactable = _petNeed.UsageCondition;
-            }
+        }
+
+        IEnumerator ShowArrowEnumerator()
+        {
+            _canPlayAgain = false;
+            _arrow.enabled = true;
+            _animator.Play("petNeed_arrow", 0, 0);
+            yield return new WaitForSeconds(_animator.GetCurrentAnimatorClipInfo(0)[0].clip.length);
+            _arrow.enabled = false;
+            _canPlayAgain = true;
         }
 
         public void SetNeed(PetNeed need)
