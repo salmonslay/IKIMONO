c87158e70f8612419affacc15c33a7d496a15e25
Arxhlight <102960157+Arxhlight@users.noreply.github.com>
2022-05-19 15:16:12 +0200

    testscript disabeled
    för att inte störa

--- Assets/Klicker.cs
+++ Assets/Klicker.cs
@@ -1,34 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Klicker : MonoBehaviour
-{
+//public class Klicker : MonoBehaviour
+//{
    
-    void Start()
-    {
-        AudioManager.Instance.PlaySound("Music", "Two");
-        AudioManager.Instance.PlaySound("DayAmb", "One");
-    }
+//    void Start()
+//    {
+//        AudioManager.Instance.PlaySound("Music", "Two");
+//        AudioManager.Instance.PlaySound("DayAmb", "One");
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Input.GetButtonDown("Jump"))
+//        {
 
-            AudioManager.Instance.RandomizeSound("Bubbles");
-            //AudioManager.Instance.RandomizeSound("Happy");
-            //FindObjectOfType<AudioManager>().randomizeSound("Eating");
-            //AudioManager.Instance.playSound("Jump", "One"); 
+//            AudioManager.Instance.RandomizeSound("Bubbles");
+//            //AudioManager.Instance.RandomizeSound("Happy");
+//            //FindObjectOfType<AudioManager>().randomizeSound("Eating");
+//            //AudioManager.Instance.playSound("Jump", "One"); 
 
-            Debug.Log("Spacetestknappfungerar");
-        }
-        if (Input.GetButtonDown("Submit"))
-        {
-            Debug.Log("Return/Enter key pressed");
-            AudioManager.Instance.StopSound("Music", "Two");
-        }
+//            Debug.Log("Spacetestknappfungerar");
+//        }
+//        if (Input.GetButtonDown("Submit"))
+//        {
+//            Debug.Log("Return/Enter key pressed");
+//            AudioManager.Instance.StopSound("Music", "Two");
+//        }
 
-    }
-}
+//    }
+//}
