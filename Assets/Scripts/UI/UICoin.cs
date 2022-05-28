40ef7e1362b0a76de69ddffdbb910b322098c022
Arxhlight <102960157+Arxhlight@users.noreply.github.com>
2022-05-28 15:38:45 +0200

    Coinanimationljud

--- Assets/Scripts/UI/UICoin.cs
+++ Assets/Scripts/UI/UICoin.cs
@@ -5,14 +5,27 @@ namespace IKIMONO.UI
     public class UICoin : MonoBehaviour
     {
         private Vector3 _endPos;
-        
+
+
+        private void Start()
+        {
+            AudioManager.Instance.RandomizeSound("Swoosh");
+        }
         private void Update()
         {
             transform.position = Vector3.MoveTowards(transform.position, _endPos, Time.deltaTime * 100);
+
+            if (transform.position == _endPos)
+            {
+                AudioManager.Instance.RandomizeSound("Coin");
+            }
+
+
             
             if (transform.position == _endPos)
             {
                 Pet.Player.Instance.AddCoins(1);
+                         
                 Destroy(gameObject);
             }
         }
